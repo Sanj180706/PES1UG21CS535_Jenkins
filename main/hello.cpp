59b53f9f34a062ad21d28a74c57fa97fd098d88c
sanj180706 <sanj180706@gmail.com>
2024-03-07 18:03:57 +0530

    Second modification

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "PES1UG21CS535\n";
+  cout << "PES1UG21CS535 Sanjay Sunil\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
